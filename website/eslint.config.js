a3dd33e6680679ee55fbe3f994ab0b99ebfbcf70
jean-smaug <maximeblanc.dev@gmail.com>
2026-06-30 12:32:33 +0200

    chore(eslint-config): update eslint to latest version and apply it to website

--- website/eslint.config.js
+++ website/eslint.config.js
@@ -1,25 +1,11 @@
-import js from "@eslint/js";
+import reactConfig from "@loopress/eslint-config/react";
 import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  { ignores: ["dist", ".output", ".vinxi"] },
+export default [
+  { ignores: [".astro", ".vercel", "dist"] },
+  ...reactConfig,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
     rules: {
-      ...reactHooks.configs.recommended.rules,
       "no-restricted-imports": [
         "error",
         {
@@ -32,9 +18,8 @@ export default tseslint.config(
           ],
         },
       ],
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
   eslintPluginPrettier,
-);
+];
