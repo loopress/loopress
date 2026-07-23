c653abf208ae7de290cd57745da5a267d0916571
jean-smaug <maximeblanc.dev@gmail.com>
2026-07-23 13:18:15 +0200

    Revert "with json schema"
    This reverts commit 8ce2594515ff8864a38fb7ab3927ecdbf52a1a5d.

--- wordpress-plugin/tests/Stubs/FakeClientException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Loopress\Tests\Stubs;
-
-use Psr\Http\Client\ClientExceptionInterface;
-
-class FakeClientException extends \RuntimeException implements ClientExceptionInterface {}
